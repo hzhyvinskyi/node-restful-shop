7ba212fac0e5199da88c00f8e6fc940f5d1b5240
hzhyvinskyi <hzhyvinskyi@gmail.com>
2019-02-09 12:09:15 +0200

    Added mongoose and connection

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const ordersRoutes = require('./api/routes/orders');
 const productRoutes = require('./api/routes/products');
+
+const db = require('./config/db');
+
+mongoose.connect(db.mongoURI, {
+    useNewUrlParser: true
+})
+  .then(() => console.log('MongoDB was successfully connected'))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
